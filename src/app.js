e465860921460e0b752f27a201ad5953a09b9ee9
Dariopach <dariopach@hotmail.com>
2023-09-04 17:47:46 -0300

    3.1

--- src/app.js
+++ src/app.js
@@ -4,12 +4,19 @@ import productRoutes from "./routes/productRoutes.js";
 import viewsRouter from "./routes/viewsRouter.js";
 import __dirname from "./utils/constantsUtil.js";
 import mongoose from "mongoose";
+import http from 'http';
+import { Server } from "socket.io";
 import cartsRouter from "./routes/cartsRouter.js";
+import { messageModel } from "./models/messageModel.js";
 
 const uri = "mongodb://127.0.0.1:27017/ecommerce";
 mongoose.connect(uri);
 
 const app = express();
+const server = http.createServer(app);
+
+//Socket.io config
+const io = new Server(server);
 
 //Handlebars config
 app.engine("handlebars", handlebars.engine());
@@ -26,7 +33,31 @@ app.use("/api/product", productRoutes);
 app.use("/products", viewsRouter);
 app.use("/api/carts", cartsRouter);
 
+
+// Configura la comunicación de Socket.IO
+io.on('connection', (socket) => {
+  console.log('Un cliente se ha conectado');
+
+  // Maneja el evento cuando un cliente envía un mensaje
+  socket.on('chatMessage', async (data) => {
+      const { user, message } = data;
+
+      // Guarda el mensaje en MongoDB 
+      const newMessage = new messageModel({ user, message });
+      await newMessage.save();
+
+      // Emite el mensaje a todos los clientes conectados
+      io.emit('message', { user, message });
+  });
+
+  // Maneja la desconexión de un cliente
+  socket.on('disconnect', () => {
+      console.log('Un cliente se ha desconectado');
+  });
+});
+
 const PORT = 8080;
 app.listen(PORT, () => {
   console.log(`Start server in PORT ${PORT}`);
 });
+
