07b7838b4e35367cf37bb6956f09050e3fd4912e
Dariopach <dariopach@hotmail.com>
2023-09-11 17:01:55 -0300

    5.3

--- src/routes/viewsRouter.js
+++ src/routes/viewsRouter.js
@@ -6,23 +6,20 @@ const ProductService = new productDBService();
 
 router.get('/', async (req,res) => {
     const { page, limit, sort, query } = req.query;
+    const result = await ProductService.getAllProducts({ page, limit, sort, query });
+
+    result.prevLink = result.hasPrevPage?`http://localhost:8080/products?page=${result.prevPage}`:'';
+    result.nextLink = result.hasNextPage?`http://localhost:8080/products?page=${result.nextPage}`:'';
+    result.isValid= !(page<=0||page>result.totalPages)
+
     res.render(
         'index',
         {
             title: 'Entrega',
             style: 'index.css',
-            products: (await ProductService.getAllProducts({ page, limit, sort, query })).products
+            result
         }
     )
 });
-
-router.get('/', async (req, res) => {
-    res.render('productList', {
-        title: 'Lista de Productos',
-        style: 'productList.css',
-        products,
-    });
-});
-
 
 export default router;
