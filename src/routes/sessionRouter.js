ba5ddc633beaf736ef6e7dbba69475b6221ec22c
Dariopach <dariopach@hotmail.com>
2023-10-30 18:07:49 -0300

    9.3

--- src/routes/sessionRouter.js
+++ src/routes/sessionRouter.js
@@ -1,25 +1,30 @@
 import { Router } from "express";
 import passport from "passport";
 
-
 const router = Router();
 
-router.get('/current', (req, res, next) => {passport.authenticate('jwt', function (err, user, info) {
-    if (err) return nest(err);
-    if(!user) {
-        return res.status(401).send({
-            error: info.messages ? info.messages : info.toString()
-        })
+router.get('/current', (req, res, next) => {
+  passport.authenticate('jwt', function (err, user, info) {
+    if (err) return next(err);
+    if (!user) {
+      return res.status(401).send({
+        error: info.messages ? info.messages : info.toString()
+      });
     }
 
-    req.user = user;
+    const userDTO = {
+      id: user.email,
+      username: user.first_name, 
+    };
+
+    req.user = userDTO; 
     next();
-})(req, res, next)} , (req, res) => {    
-    
-    res.send({
-        status: 'success',
-        payload: req.user
-    });
+  })(req, res, next);
+}, (req, res) => {
+  res.send({
+    status: 'success',
+    payload: req.user
+  });
 });
 
 export default router;
